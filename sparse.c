ba3940fe514a1d452cf29815ff898aac03ac70d2
AnandhuKrisB <anandhukrishna10b@gmail.com>
2025-08-11 11:44:06 +0530

    Starting stack

--- sparse.c
+++ sparse.c
@@ -64,8 +64,8 @@ int main() {
     }
 
     for(int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            sum[i][j] = a[i][j] + b[i][j];
+        for (int j = 0; j < 3; j++) { 
+             
         }
         printf("\n");
     }
